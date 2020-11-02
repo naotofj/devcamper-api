f2e371801a5f7d4247682d11ba7c61053bf1729f
naoto.fj <naoto.fj@gmail.com>
2020-11-02 10:15:07 -0300

    Bootcamps API CRUD and basic moongoose config

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Conect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
+
+// Specify a port and start server
 const PORT = process.env.PORT || 5000;
 
-// run a server
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => (process.exitCode = 1));
+});
